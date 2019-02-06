9fd8432f62565db9445edfc66747112af3f8c5b5
Remach <remach@gmail.com>
2019-02-06 23:02:34 +0300

    Correct static settings

--- number/urls.py
+++ number/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import routers
 from number import views
@@ -31,5 +33,5 @@ urlpatterns = [
     #path('^number/(?P<value>.+)/$', views.NumberList.as_view()),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
