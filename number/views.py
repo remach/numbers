60d924bfa7b56b79a66399cf8488c1e8fe5c750f
remach@gmail.com <remach@gmail.com>
2019-02-11 17:51:40 +0300

    list

--- number/views.py
+++ number/views.py
@@ -9,13 +9,16 @@ class NumberViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    value =1000# self.request.query_params.get('value', None)
-    queryset = Number.objects.extra(select={'d_field': '{} - value'.format(value)}).filter(value__range=(value-100,value+100)).order_by('d_field')
+    #value =1000# self.request.query_params.get('value', None)
+    #queryset = Number.objects.extra(select={'d_field': '{} - value'.format(value)}).filter(value__range=(value-100,value+100)).order_by('d_field')
     serializer_class = NumberSerializer
     
     def get_queryset(self):
         value = self.request.query_params.get('value', None)
-        queryset = Number.objects.extra(select={'d_field': 'abs({} - value)'.format(value)}).order_by('d_field')[:1]
+        if value == None:
+            queryset = Number.objects.all()
+        else: 
+            queryset = Number.objects.extra(select={'d_field': 'abs({} - value)'.format(value)}).order_by('d_field')[:1]
         return queryset
 
 # Create your views here.
