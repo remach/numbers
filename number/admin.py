bf043fb1d6fa4ba4d3678de3aa0f2341b1b72421
remach@gmail.com <remach@gmail.com>
2019-02-12 16:07:16 +0300

    pg_dump and psql. Add two fields to Entity.

--- number/admin.py
+++ number/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import  Number
 
 class NumberAdmin(admin.ModelAdmin):
-    fields = ['description_text', 'value']
+    fields = ['description_text', 'value','unit','link','pub_date']
+    list_display = ('description_text', 'value','unit','link','pub_date')
+    list_filter = ['unit']
+
     
 admin.site.register(Number)
--- number/models.py
+++ number/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
+from django.utils import timezone
 
 # Create your models here.
 class Number(models.Model):
     value = models.DecimalField(max_digits=999, decimal_places=2)
     description_text = models.CharField(max_length=500)
     unit = models.CharField(max_length=10)
+    pub_date = models.DateTimeField('Date published',default=timezone.now)
+    link = models.URLField('Url',default='#')
     def __str__(self):              # __unicode__ on Python 2
         return '{} - {}{}'.format( self.description_text,self.value,self.unit)
